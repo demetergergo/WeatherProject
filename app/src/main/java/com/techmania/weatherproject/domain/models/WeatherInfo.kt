deeaae33b90bd76802fed43f26c65c4759a946e0
gergo.demeter <demetergergo6@gmail.com>
2024-08-21 13:27:29 +0200

    Added new string resource for weather types

--- app/src/main/java/com/techmania/weatherproject/domain/models/WeatherInfo.kt
+++ app/src/main/java/com/techmania/weatherproject/domain/models/WeatherInfo.kt
@@ -2,12 +2,12 @@ package com.techmania.weatherproject.domain.models
 
 import java.time.LocalDateTime
 
-data class WeatherInfo (
+data class WeatherInfo(
     val time: LocalDateTime,
     val temperature: Double,
     val apparentTemperature: Double,
     val precipitation: Double,
-    val weatherDesc: String,
+    val weatherDesc: Int,
     val iconRes: Int,
     val windSpeed: Double,
 )
