264f1f562c7d531892f89e8657290bb924d9ef15
gergo.demeter <demetergergo6@gmail.com>
2024-09-19 14:40:53 +0200

    Fixed bug where theme was set twice on startup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,4 +99,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/techmania/weatherproject/presentation/MainActivity.kt
+++ app/src/main/java/com/techmania/weatherproject/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.techmania.weatherproject.data.dataStore.MyDataStore
@@ -12,7 +13,9 @@ import com.techmania.weatherproject.presentation.navigation.SetupNavGraph
 import com.techmania.weatherproject.presentation.ui.theme.WeatherProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,22 +24,27 @@ class MainActivity : AppCompatActivity() {
         MyDataStore.loadFlow("darkMode", this)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        lifecycleScope.launch {
+        val splashScreen = installSplashScreen()
+        val isDarkMode = runBlocking {
             MyDataStore.ensureInitialized("darkMode", "true", this@MainActivity)
+            preferences.first().toBoolean()
+        }
+        setDarkMode(isDarkMode)
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
             preferences.collect { value ->
                 setDarkMode(value.toBoolean())
             }
         }
+        enableEdgeToEdge()
         setContent {
             WeatherProjectTheme(dynamicColor = true) {
                 val navController = rememberNavController()
                 SetupNavGraph(navController = navController)
             }
+
         }
     }
-
 }
 
 private fun setDarkMode(isDarkMode: Boolean) {
